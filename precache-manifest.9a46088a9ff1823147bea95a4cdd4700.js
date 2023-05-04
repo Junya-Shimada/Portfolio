32699eeec2377937c104c4c3f7df599863f8a0f4
sen-bero <yakuzen555@gmail.com>
2023-05-04 12:01:28 +0900

    Updates

---
+++ precache-manifest.9a46088a9ff1823147bea95a4cdd4700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c2de297766a5e54a53d",
+    "url": "/portfolio/./static/css/main.9828e6c3.chunk.css"
+  },
+  {
+    "revision": "9c2de297766a5e54a53d",
+    "url": "/portfolio/./static/js/main.9c2de297.chunk.js"
+  },
+  {
+    "revision": "068b0156258b7769bbe9",
+    "url": "/portfolio/./static/js/1.068b0156.chunk.js"
+  },
+  {
+    "revision": "46e43111dcad03a6e4c4",
+    "url": "/portfolio/./static/js/2.46e43111.chunk.js"
+  },
+  {
+    "revision": "0152fdd96fbf1b0ef95f",
+    "url": "/portfolio/./static/js/runtime~main.0152fdd9.js"
+  },
+  {
+    "revision": "74a83c0fab9cc2eb61052aa4f2c19d00",
+    "url": "/portfolio/./index.html"
+  }
+];
