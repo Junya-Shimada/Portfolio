c6acc6b8a2d95214894e387577274ae1769fea98
sen-bero <yakuzen555@gmail.com>
2023-05-24 16:10:12 +0900

    Updates

---
+++ precache-manifest.f8e1c74948464e1d8c730501f8a79745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac4197e64b54341345ab",
+    "url": "/portfolio/static/css/main.9828e6c3.chunk.css"
+  },
+  {
+    "revision": "ac4197e64b54341345ab",
+    "url": "/portfolio/static/js/main.ac4197e6.chunk.js"
+  },
+  {
+    "revision": "068b0156258b7769bbe9",
+    "url": "/portfolio/static/js/1.068b0156.chunk.js"
+  },
+  {
+    "revision": "46e43111dcad03a6e4c4",
+    "url": "/portfolio/static/js/2.46e43111.chunk.js"
+  },
+  {
+    "revision": "a12d1cc87555c826755b",
+    "url": "/portfolio/static/js/runtime~main.a12d1cc8.js"
+  },
+  {
+    "revision": "dceadb15784c8a2458b5157ae47f842b",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/./precache-manifest.01b690c0c243030996197cc67c9ba05e.js"
+  "/portfolio/precache-manifest.f8e1c74948464e1d8c730501f8a79745.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/./index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
